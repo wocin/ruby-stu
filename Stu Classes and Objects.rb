a9c44e145279c063aa4b0ad4639bbd4148f84b96
wocin <925511765@qq.com>
2014-02-25 00:56:10 +0800

    add

---
+++ Stu Classes and Objects.rb
@@ -0,0 +1,26 @@
+=begin
+全局变量
+$
+
+实例变量(对象变量)
+@	关联与当前的对象
+
+类变量
+@@
+
+类方法	self前缀
+实例方法
+=end
+
+=begin
+继承
+<
+
+反射
+
+instance_variables实例变量
+protected_methods保护方法
+private_methods私有方法
+public_methods公共方法
+
+=end
